69789f334004e6964e46809860673ca69cb828ca
Gorka Irazoqui <gorka.irazoki@gmail.com>
2021-02-19 10:24:23 +0100

    remove blockbuilder API error

--- client/consensus/src/lib.rs
+++ client/consensus/src/lib.rs
@@ -84,7 +84,6 @@ impl<B, I, C> FrontierBlockImport<B, I, C> where
 	I::Error: Into<ConsensusError>,
 	C: ProvideRuntimeApi<B> + Send + Sync + HeaderBackend<B> + AuxStore + ProvideCache<B> + BlockOf,
 	C::Api: EthereumRuntimeRPCApi<B>,
-	C::Api: BlockBuilderApi<B, Error = sp_blockchain::Error>,
 	C::Api: BlockBuilderApi<B>,
 {
 	pub fn new(
