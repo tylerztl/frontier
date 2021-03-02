b931718717b908db8c659215df2af80ff50d15bc
Stephen Shelton <steve@brewcraft.org>
2021-03-02 13:21:52 -0700

    Make modexp benchmark testable

--- frame/evm/src/benchmarking.rs
+++ frame/evm/src/benchmarking.rs
@@ -17,13 +17,13 @@
 #![cfg(feature = "runtime-benchmarks")]
 
 use super::*;
+use crate::Config;
 
 use sp_std::prelude::*;
-use frame_system::{RawOrigin, Origin};
-use frame_benchmarking::{benchmarks, account, whitelisted_caller};
+use frame_benchmarking::{benchmarks};
 
-use crate::Module as Evm;
 use pallet_evm_precompile_modexp::Modexp;
+use num::{BigUint, FromPrimitive};
 
 extern crate hex;
 
@@ -45,38 +45,63 @@ benchmarks! {
 	}
 	*/
 
-	test_modexp {
-		let input = hex::decode(
-			"0000000000000000000000000000000000000000000000000000000000000001\
-			0000000000000000000000000000000000000000000000000000000000000020\
-			0000000000000000000000000000000000000000000000000000000000000020\
-			03\
-			fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2e\
-			fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f")
+
+
+	modexp {
+
+		let b in 0 .. 10_000;
+		let m in 0 .. 10_000;
+		let e in 0 .. 10_000;
+
+		// first 3 args declare the size of the following 3 args. we hard code these
+		// and then build the following 3 args
+		// TODO: don't hard code
+		// TODO: see if large values impact runtime
+		let size_args = hex::decode(
+			"0000000000000000000000000000000000000000000000000000000000000100\
+			0000000000000000000000000000000000000000000000000000000000000100\
+			0000000000000000000000000000000000000000000000000000000000000100")
 			.expect("Decode failed");
 
-		// let input: [u8; 100] = [0; 100];
+		assert!(size_args.len() == 96);
+
+		// TODO: use benchmarking initialize fn
+		let base_big = BigUint::from_u32(b).unwrap();
+		let mod_big = BigUint::from_u32(m).unwrap();
+		let exp_big = BigUint::from_u32(e).unwrap();
+
+		let base_bytes = base_big.to_bytes_be();
+		let mod_bytes = mod_big.to_bytes_be();
+		let exp_bytes = exp_big.to_bytes_be();
+
+		let mut input: [u8; (32 * 6)] = [0; (32 * 6)]; // room for 6 256 bit uints
+
+		input[..96].copy_from_slice(&size_args);
+		input[(96+(32*1))..].copy_from_slice(&base_bytes);
+		input[(96+(32*2))..].copy_from_slice(&mod_bytes);
+		input[(96+(32*3))..].copy_from_slice(&exp_bytes);
 
 		let cost: u64 = 1;
-		<Modexp as LinearCostPrecompile>::execute(&input, cost);
 
 	}:
-	{ }
+	{
+		<Modexp as LinearCostPrecompile>::execute(&input, cost)
+			.expect("Modexp::execute() failed");
+	} verify {
+	}
 
 }
 
 #[cfg(test)]
 mod tests {
 	use super::*;
+	use frame_support::{assert_ok};
 	use crate::tests::{new_test_ext, Test};
-	use frame_support::{assert_ok, assert_err};
 
-	/*
 	#[test]
 	fn test_benchmarks() {
 		new_test_ext().execute_with(|| {
-			assert_ok!(test_benchmark_call::<Test>());
+			assert_ok!(test_benchmark_modexp::<Test>());
 		});
 	}
-	*/
 }
