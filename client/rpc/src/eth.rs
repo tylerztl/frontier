70fdc4b17a52c561dcece298a5292835773e144a
librelois <c@elo.tf>
2021-08-02 14:17:13 +0200

    fix(binary search): use gas limit instead of 1 billion for upper bound

--- client/rpc/src/eth.rs
+++ client/rpc/src/eth.rs
@@ -902,7 +902,7 @@ impl<B, C, P, CT, BE, H: ExHashT> EthApiT for EthApi<B, C, P, CT, BE, H> where
 		if cfg!(feature = "rpc_binary_search_estimate") {
 			let mut lower = U256::from(21_000);
 			// TODO: get a good upper limit, but below U64::max to operation overflow
-			let mut upper = U256::from(1_000_000_000);
+			let mut upper = U256::from(gas_limit);
 			let mut mid = upper;
 			let mut best = mid;
 			let mut old_best: U256;
