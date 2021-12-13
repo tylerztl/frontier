fde006f9cce571e6f114fe65e5209839a9b1f0ba
tyler <570697260@qq.com>
2021-12-13 13:30:43 +0800

    modify spec name

--- template/runtime/src/lib.rs
+++ template/runtime/src/lib.rs
@@ -82,8 +82,8 @@ pub mod opaque {
 }
 
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("node-frontier-template"),
-	impl_name: create_runtime_str!("node-frontier-template"),
+	spec_name: create_runtime_str!("usm"),
+	impl_name: create_runtime_str!("usm"),
 	authoring_version: 1,
 	spec_version: 1,
 	impl_version: 1,
